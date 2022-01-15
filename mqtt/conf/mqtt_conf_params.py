331e26e3475065b29d8632f1cc87c5241b28b0cb
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-15 12:50:52 +0100

    added water parameters

--- mqtt/conf/mqtt_conf_params.py
+++ mqtt/conf/mqtt_conf_params.py
@@ -2,4 +2,7 @@ class MqttConfigurationParams(object):
     BROKER_ADDRESS = "127.0.0.1"
     BROKER_PORT = 1883
     # TODO add authentication configuration
-    MQTT_DEFAULT_TOPIC = ""
+    MQTT_DEFAULT_TOPIC = "metering"
+    WATER_DEVICE_TOPIC = "water/device"
+    TELEMETRY_TOPIC = "telemetry"
+    COMMAND_TOPIC = "command"
