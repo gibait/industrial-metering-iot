9d91b188cabbc921fbe2b34a28d61127f002a97c
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-15 12:53:06 +0100

    added optional timestamp due to monitor behaviour

--- mqtt/message/telemetry_message.py
+++ mqtt/message/telemetry_message.py
@@ -3,11 +3,11 @@ import time
 import json
 
 
-class TelemetryMessage:
-    def __init__(self, message_type, value):
-        self.timestamp = int(time.time())
-        self.type = message_type
+class TelemetryMessage(object):
+    def __init__(self, type, value, **kwargs):
+        self.type = type
         self.value = value
+        self.timestamp = kwargs.get('timestamp', None) if kwargs.get('timestamp', None) else int(time.time())
 
     def to_json(self):
         return json.dumps(self, default=lambda o: o.__dict__)
