6801e943bbf46932a1d21a1bff2ba0d967104c49
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-17 10:38:45 +0100

    now checks if active

--- mqtt/resource/water_sensor_resource.py
+++ mqtt/resource/water_sensor_resource.py
@@ -22,13 +22,14 @@ class WaterSensorResource(SmartObjectResource):
         super().__init__(uuid.uuid4(), self.RESOURCE_TYPE)
         self.timestamp = int(time.time())
         self.water_level = MIN_WATER_FLOW + random.uniform(MIN_WATER_FLOW, MAX_WATER_FLOW) * random.uniform(0, 1)
+        self.is_active = True
         # self.start_periodic_event_value_update_task()
 
     async def update_water_level(self):
         self.water_level = self.water_level + (MIN_INCREASE + MAX_INCREASE * random.uniform(0, 1))
         await self.notify_update(self.type, self.water_level)
-        # await asyncio.sleep(UPDATE_DELAY)
         # TODO check if value < 0
+        # TODO maybe add slow decrementing when not active
 
     async def start_periodic_event_value_update_task(self):
         try:
@@ -37,10 +38,16 @@ class WaterSensorResource(SmartObjectResource):
             time.sleep(TASK_DELAY)
             while True:
                 await asyncio.sleep(UPDATE_DELAY)
-                await self.update_water_level()
+                if self.is_active:
+                    await self.update_water_level()
+                else:
+                    print("Supply is switched OFF")
         except Exception as e:
             print("Error starting periodic event value update!")
             print(e)
+
+    def set_active(self, type, updated_value):
+        self.is_active = updated_value
 
 def on_data_changed(water_level):
     print(f"New Water Level: {water_level} l/s")
