2d19100c614363b115e15cdd6f56367b1349ad45
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-17 10:32:18 +0100

    changed actuator functioning

--- mqtt/resource/generic_actuator_resource.py
+++ mqtt/resource/generic_actuator_resource.py
@@ -2,17 +2,16 @@ import uuid
 
 from mqtt.resource.smart_object_resource import SmartObjectResource
 
-RESOURCE_TYPE = "iot:actuator:generic"
-
-
 TASK_DELAY = 2 #seconds
 UPDATE_DELAY = 3 #seconds
 
-
 class GenericActuatorResource(SmartObjectResource):
+
+    RESOURCE_TYPE = "iot:actuator:generic"
+
     def __init__(self):
-        super().__init__(uuid.uuid4(), RESOURCE_TYPE)
-        self.value = False
+        super().__init__(uuid.uuid4(), self.RESOURCE_TYPE)
+        self.value = True
 
     async def switch_actuator_status(self):
         self.value = not self.value
