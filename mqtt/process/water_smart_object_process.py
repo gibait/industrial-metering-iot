e9212fbaf427c16b805353beb6695aa1de1a8258
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-17 10:37:12 +0100

    not establishing connection now

--- mqtt/process/water_smart_object_process.py
+++ mqtt/process/water_smart_object_process.py
@@ -11,16 +11,12 @@ class WaterSmartObjectProcess:
     def __init__(self):
         self.device_id = uuid.uuid4()
         self.mqtt_client = mqtt.Client(str(self.device_id), clean_session=True)
-        self.mqtt_client.connect(
-            host=mqttParams.BROKER_ADDRESS,
-            port=mqttParams.BROKER_PORT,
-            keepalive=10)
+
         try:
             water_metering_smart_object = WaterMeteringSmartObject(self.device_id, self.mqtt_client, {
                 "sensor" : WaterSensorResource(),
                 "actuator" : GenericActuatorResource()
             })
-
             water_metering_smart_object.start()
         except Exception as e:
             print(e)
