61841d52c141012854f07e574c85b6676922c967
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-20 15:41:01 +0100

    minor changes

--- mqtt/resource/gas_sensor_resource.py
+++ mqtt/resource/gas_sensor_resource.py
@@ -6,38 +6,31 @@ import logging
 
 
 from mqtt.resource.smart_object_resource import SmartObjectResource
+from mqtt.conf.sensor_conf_values import GasConfValues as sensValues
 
 
 class GasSensorResource(SmartObjectResource):
 
-    RESOURCE_TYPE = "iot:sensor:gas"
-    MAX_GAS_USAGE = 10.0
-    MIN_GAS_USAGE = 1.0
-    MAX_INCREASE = 0.5
-    MIN_INCREASE = 0.1
-    TASK_DELAY = 2  # seconds
-    UPDATE_DELAY = 5  # seconds
+    RESOURCE_TYPE = sensValues.RESOURCE_TYPE
 
     def __init__(self):
         super().__init__(uuid.uuid4(), self.RESOURCE_TYPE)
         self.timestamp = int(time.time())
-        self.gas_level = self.MIN_GAS_USAGE + random.uniform(self.MIN_GAS_USAGE, self.MAX_GAS_USAGE) * random.uniform(0, 1)
+        self.gas_level = sensValues.MIN_USAGE + random.uniform(sensValues.MIN_USAGE, sensValues.MAX_USAGE) * random.uniform(0, 1)
         self.is_active = True
-        # self.start_periodic_event_value_update_task()
 
     async def update_gas_level(self):
-        self.gas_level = self.gas_level + (self.MIN_INCREASE + self.MAX_INCREASE * random.uniform(0, 1))
-        await self.notify_update(self.type, self.gas_level)
+        self.gas_level = self.gas_level + (sensValues.MIN_INCREASE + sensValues.MAX_INCREASE * random.uniform(0, 1))
+        await self.notify_update(self.gas_level, type=self.type, unit=sensValues.UNIT)
         # TODO check if value < 0
         # TODO maybe add slow decrementing when not active
 
     async def start_periodic_event_value_update_task(self):
         try:
-            logging.info(f"Starting periodic Update Task with period {self.UPDATE_DELAY}s")
-            # self.add_data_listener(on_data_changed)
-            time.sleep(self.TASK_DELAY)
+            logging.info(f"Starting periodic Update Task with period {sensValues.UPDATE_DELAY}s")
+            await asyncio.sleep(sensValues.TASK_DELAY)
             while True:
-                await asyncio.sleep(self.UPDATE_DELAY)
+                await asyncio.sleep(sensValues.UPDATE_DELAY)
                 if self.is_active:
                     await self.update_gas_level()
                 else:
@@ -46,5 +39,5 @@ class GasSensorResource(SmartObjectResource):
             logging.error("Error starting periodic event value update!")
             logging.error(e)
 
-    def set_active(self, type, updated_value):
+    def set_active(self, updated_value, **kwargs):
         self.is_active = updated_value
--- mqtt/resource/water_sensor_resource.py
+++ mqtt/resource/water_sensor_resource.py
@@ -4,38 +4,31 @@ import time
 import uuid
 import logging
 
-
 from mqtt.resource.smart_object_resource import SmartObjectResource
-
+from mqtt.conf.sensor_conf_values import WaterConfValues as sensValues
 
 class WaterSensorResource(SmartObjectResource):
 
-    RESOURCE_TYPE = "iot:sensor:water"
-    MAX_WATER_FLOW = 50.0
-    MIN_WATER_FLOW = 10.0
-    MAX_INCREASE = 1.0
-    MIN_INCREASE = 0.1
-    TASK_DELAY = 2  # seconds
-    UPDATE_DELAY = 5  # seconds
+    RESOURCE_TYPE = sensValues.RESOURCE_TYPE
 
     def __init__(self):
         super().__init__(uuid.uuid4(), self.RESOURCE_TYPE)
         self.timestamp = int(time.time())
-        self.water_level = self.MIN_WATER_FLOW + random.uniform(self.MIN_WATER_FLOW, self.MAX_WATER_FLOW) * random.uniform(0, 1)
+        self.water_level = sensValues.MIN_USAGE + random.uniform(sensValues.MIN_USAGE, sensValues.MAX_USAGE) * random.uniform(0, 1)
         self.is_active = True
 
     async def update_water_level(self):
-        self.water_level = self.water_level + (self.MIN_INCREASE + self.MAX_INCREASE * random.uniform(0, 1))
-        await self.notify_update(self.type, self.water_level)
+        self.water_level = self.water_level + (sensValues.MIN_INCREASE + sensValues.MAX_INCREASE * random.uniform(0, 1))
+        await self.notify_update(self.water_level, type=self.type, unit=sensValues.UNIT)
         # TODO check if value < 0
         # TODO maybe add slow decrementing when not active
 
     async def start_periodic_event_value_update_task(self):
         try:
-            logging.info(f"Starting periodic Update Task with period {self.UPDATE_DELAY}s")
-            time.sleep(self.TASK_DELAY)
+            logging.info(f"Starting periodic Update Task with period {sensValues.UPDATE_DELAY}s")
+            await asyncio.sleep(sensValues.TASK_DELAY)
             while True:
-                await asyncio.sleep(self.UPDATE_DELAY)
+                await asyncio.sleep(sensValues.UPDATE_DELAY)
                 if self.is_active:
                     await self.update_water_level()
                 else:
@@ -44,5 +37,5 @@ class WaterSensorResource(SmartObjectResource):
             logging.error("Error starting periodic event value update!")
             logging.error(e)
 
-    def set_active(self, type, updated_value):
+    def set_active(self, updated_value, **kwargs):
         self.is_active = updated_value
