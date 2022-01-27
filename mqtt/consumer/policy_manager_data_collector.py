b04e08ab84b4a9f5df5b13be1862020e95743b67
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-27 16:00:55 +0100

    added data collector part, commented & renamed

--- mqtt/consumer/policy_manager_data_collector.py
+++ mqtt/consumer/policy_manager_data_collector.py
@@ -1,7 +1,10 @@
 import asyncio
 import json
+import os
 import uuid
 import logging
+
+import csv
 import paho.mqtt.client as mqtt
 
 from asyncio_mqtt import Client, MqttError
@@ -15,23 +18,35 @@ from mqtt.resource.electricity_sensor_resource import ElectricitySensorResource
 logging.basicConfig(level=logging.DEBUG,
                     format="%(asctime)s %(levelname)-8s %(message)s")
 
-class PolicyManager:
+DEMO_RUNTIME = 60 #s
+
+class PolicyManagerAndDataCollector:
     """
-    Generic Resource Consumer class handling every kind of resource
+    Policy Manager and Data Collector
     Load threshold data from a file
-    Is started by an async start function
-    Uses logging
+    On message saves new value or checks if above threshold
+    Sends control message in case
+    Schedule control message to resume operation
+    Stops after DEMO_RUNTIME seconds
+    Saves records to csv file
     """
+
+    # Craft data file path for csv
+    data_folder = "../../data"
+    data_file = "industrial_metering_consumption.csv"
+    file_path = os.path.join(data_folder, data_file)
 
     # Load delay times from json config file
     with open('../conf/policy_manager_params.json') as json_file:
         config_values = json.load(json_file)
 
+    # Keep track of notified threshold-reached devices
     is_resource_threshold_notified = {}
 
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.resource_value_history = {}
+        self.senml_data_array = []
         # Using Client class from async-mqtt wrapper
         self.mqtt_client = Client(
             hostname=mqttParams.BROKER_ADDRESS,
@@ -54,11 +69,11 @@ class PolicyManager:
     async def subscribe_to_telemetry_topic(self):
         """
         Subscribe to every telemetry topic available using mqtt wildcards
-        /metering/[resource]/device/[unique_id]/(control)(telemetry)
+        /metering/[location]/[plant]/[resource]/device/[unique_id]/(control)(telemetry)
         """
         try:
             # Subscribe to all telemetry topic available
-            device_telemetry_topic = "{0}/+/{1}/+/{2}".format(
+            device_telemetry_topic = "{0}/+/+/+/{1}/+/{2}".format(
                 mqttParams.MQTT_DEFAULT_TOPIC,
                 mqttParams.DEVICE_TOPIC,
                 mqttParams.TELEMETRY_TOPIC
@@ -86,6 +101,7 @@ class PolicyManager:
                     telemetry_message:TelemetryMessage = self.parse_telemetry_message(message)
                     # Check for the resource type
                     if telemetry_message.type in (WaterSensorResource.RESOURCE_TYPE, GasSensorResource.RESOURCE_TYPE, ElectricitySensorResource.RESOURCE_TYPE):
+                        self.senml_data_array.append(telemetry_message.to_json())
                         new_level = telemetry_message.value
                         logging.info(f"Detected new value: {new_level} in topic {message.topic}")
 
@@ -114,6 +130,25 @@ class PolicyManager:
 
         except Exception as e:
             logging.error("Error receiving message!")
+            logging.error(e)
+
+    def write_senml_records_to_file(self):
+        """
+        Write senml records saved in list to csv file
+        header = n, bn, v, u, t
+        :return:
+        """
+        try:
+            header = list((json.loads(self.senml_data_array[0]).keys()))
+            with open(self.file_path, "w", newline="") as f:
+                cw = csv.DictWriter(f, header)
+                cw.writeheader()
+                logging.info(f"Writing header {header} to file")
+                for record in self.senml_data_array:
+                    cw.writerow(json.loads(record))
+                logging.info("Records successfully written to file")
+        except Exception as e:
+            logging.error("Error writing to file")
             logging.error(e)
 
     def refactor_telemetry_topic_to_control(self, message):
@@ -185,13 +220,32 @@ class PolicyManager:
             await self.on_connect()
             await self.subscribe_to_telemetry_topic()
             asyncio.get_event_loop().create_task(self.on_message())
-            logging.info("Data Manager started ...")
+            asyncio.get_event_loop().create_task(self.stop())
+            logging.info("Policy Manager and Data Collector started ...")
         except Exception as e:
             logging.error("Error starting Data Manager!")
             logging.error(e)
 
+    async def stop(self):
+        """
+        Sleeps for simulation time and then stops tasks and loop
+        When done starts calls write to file function
+        :return:
+        """
+        try:
+            logging.info(f"Running simulation for {DEMO_RUNTIME} seconds")
+            await asyncio.sleep(DEMO_RUNTIME)
+            for task in asyncio.all_tasks():
+                task.cancel()
+            asyncio.get_event_loop().stop()
+            logging.info("Commencing writing senml Records to file ...")
+            self.write_senml_records_to_file()
+        except Exception as e:
+            logging.error("Error interrupting task")
+            logging.error(e)
+
 def main():
-    asyncio.get_event_loop().create_task(PolicyManager().start())
+    asyncio.get_event_loop().create_task(PolicyManagerAndDataCollector().start())
     asyncio.get_event_loop().run_forever()
 
 if __name__ == '__main__':
