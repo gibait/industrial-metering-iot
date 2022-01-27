d050784f27ca3b61f071d462cfe245ed00a67cc5
Giulio Barabino <giuliobarabino99@gmail.com>
2022-01-27 15:59:50 +0100

    reworked build from json and added param name

--- mqtt/message/telemetry_message.py
+++ mqtt/message/telemetry_message.py
@@ -7,33 +7,37 @@ from kpn_senml import *
 
 
 class TelemetryMessage(object):
+
     def __init__(self, **kwargs):
         """
         Telemetry message has all optional param because of
         build_class_from_senml_json method which needs an instance of self
-        :param kwargs: type
+        :param kwargs: name (UUID)
+        :param kwargs: type iot:(sensor)(actuator):[resource]
         :param kwargs: value
         :param kwargs: unit
         :param kwargs: timestamp
         """
+        self.name = kwargs.get('name', None)
         self.type = kwargs.get('type', None)
         self.value = kwargs.get('value', None)
         self.unit = kwargs.get('unit', None) if kwargs.get('unit', None) else None
         self.timestamp = kwargs.get('timestamp', None) if kwargs.get('timestamp', None) else int(time.time())
-        self.pack = SenmlPack("telemetry")
 
     def build_senml_json_payload(self):
+        pack = SenmlPack(self.type)
         try:
-            record = SenmlRecord(name=self.type,
+            record = SenmlRecord(name=str(self.name),
                                  value=self.value,
-                                 unit=self.unit
+                                 unit=self.unit,
+                                 time=self.timestamp
                                  )
-            self.pack.add(record)
+            pack.add(record)
         except Exception as e:
             logging.error("Error Building Telemetry SenML Json payload")
             logging.error(e)
         finally:
-            return self.pack.to_json()
+            return pack.to_json()
 
     def build_class_from_senml_json(self, payload):
         """
@@ -42,11 +46,12 @@ class TelemetryMessage(object):
         :return: self
         """
         try:
-            # It does not load unit params, only name and value
-            self.pack.from_json(payload)
-            # Accessing kpn_SenmlRecord values
-            self.type = self.pack._data[0].name
-            self.value = self.pack._data[0].value
+            records = json.loads(payload)
+            for record in records:
+                self.type = record["bn"]
+                self.value = record["v"] if "v" in record else record['vb']
+                self.unit = record["u"] if "u" in record else None
+                self.name = record["n"]
         except Exception as e:
             logging.error("Error crafting Telemetry Message from json")
             logging.error(e)
@@ -54,4 +59,4 @@ class TelemetryMessage(object):
             return self
 
     def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
+        return json.dumps(self, default=lambda o: o.__dict__).replace("name", "n").replace("value", "v").replace("timestamp", "t").replace("unit", "u").replace("type", "bn")
