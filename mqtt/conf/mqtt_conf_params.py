d52e206297134120ab827a0c3ce4372edc2980b7
Giulio Barabino <giuliobarabino99@gmail.com>
2022-02-03 19:11:51 +0100

    added info topic

--- mqtt/conf/mqtt_conf_params.py
+++ mqtt/conf/mqtt_conf_params.py
@@ -1,8 +1,10 @@
 class MqttConfigurationParams(object):
     BROKER_ADDRESS = "127.0.0.1"
     BROKER_PORT = 1883
-    # TODO add authentication configuration
+    # TODO add authentication configuration by certificate for consumer
+    # TODO add authorization configuration for consumer
     MQTT_DEFAULT_TOPIC = "metering"
     DEVICE_TOPIC = "device"
     TELEMETRY_TOPIC = "telemetry"
     CONTROL_TOPIC = "control"
+    INFO_TOPIC = "info"
